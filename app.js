ee2011a2f1868d698b2924b20ecd99d4846c1ce2
Leonardo Silverio <leonardo.silverio@brf-br.com>
2019-07-11 14:52:10 -0300

    Almost finishing

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ new Vue({
   el: "#app",
   data: {
     onGame: false,
-    playerLife: 90,
-    monsterLife: 20
+    playerLife: 100,
+		monsterLife: 100,
+		logs: [],
   },
   computed: {
     hasWinner() {
@@ -12,17 +13,13 @@ new Vue({
 
   },
   methods: {
-		attack(special) {
-			this.hurt(10,5, special)
-		},
-
-		hurt(max, min, special) {
-			const bonus  = special ? 5 : 0
-			const hurt = this.randomNo(max + bonus, min + bonus)
-			this.monsterLife = Math.max(this.monsterLife - hurt,0)
-			console.log(special, hurt, this.monsterLife)
-		},
 		
+		startGame() {
+			this.onGame = true,
+			this.playerLife = 100,
+			this.monsterLife = 100
+		},
+				
 		randomNo(max, min) {
 			const value = Math.random() * (max - min) + min
 			return Math.round(value);
@@ -31,8 +28,59 @@ new Vue({
 		resetGame() {
 			this.onGame = false,
 			this.playerLife = 100,
-			this.monsterLife = 100
+			this.monsterLife = 100,
+			this.logs = []
 		},
+
+		hurt(who, max, min, special, src , trgt , cls) {
+			const bonus  = special ? 5 : 0
+			const hurt = this.randomNo(max + bonus, min + bonus)
+			this[who] = Math.max(this[who] - hurt,0)
+
+				if (special) {
+					this.registerlog(`${src} acertou o ${trgt} com uma fireball, dando dano de ${hurt}%`, cls)
+					return
+				}
+
+				if(src == 'monstrão'){
+					this.registerlog(`${src} acertou o ${trgt} de revesgueio, dando dano de ${hurt}%`, cls)
+					return
+				}
+
+				this.registerlog(`${src} acertou o ${trgt} com uma espadada, dando dano de ${hurt}%`, cls)
+		},
+
+		attack(special) {
+			this.hurt('monsterLife', 10 , 5 , special , 'jogador' , 'monstrão' , 'player' )
+			if (this.monsterLife > 0){
+				this.hurt('playerLife', 12 , 7 , false , 'monstrão' , 'jogador' , 'monster' )
+			}
+		},
+
+		healAndHurt() {
+			this.heal( 15 , 10 , 'player' )
+			this.hurt('playerLife',12,7, false , 'monstrão' , 'jogador' , 'monster' )
+		},
+
+		heal(max, min , cls){
+			const value = this.randomNo(max, min)
+			this.playerLife = Math.min(this.playerLife + value, 100)
+			this.registerlog(`Jogador se curou, aumentando a vida em ${value}%`, cls)
+		},
+
+		registerlog(text, cls) {
+			this.logs.unshift({text, cls})
+		}
+
 	},
-  watch: {}
+
+  watch: {
+		hasWinner(value){
+			if (value) {
+				this.onGame = false
+				this.logs = []
+			}
+		}
+	}
+
 })
